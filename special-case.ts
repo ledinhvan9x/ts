bc1d132d419d2a673b0e9b75bb4f1086c760a678
ledinhvan9x <99377269+ledinhvan9x@users.noreply.github.com>
2026-08-12 20:06:40 +0700

    Update special-case.ts

--- special-case.ts
+++ special-case.ts
@@ -8,3 +8,14 @@ type B = {
 
 type C = A & B; // string & number => never
 // with interface: Error
+
+type I = NonNullable<string | number | null | undefined>
+// string | number => not null and not undefined
+
+type Fn = (name: string, age: number) => boolean
+
+type J = Parameters<Fn>
+// [name: string, age: number] => Tuple type
+
+type K = ReturnType<Fn>
+// boolean => depends on return type in Fn
